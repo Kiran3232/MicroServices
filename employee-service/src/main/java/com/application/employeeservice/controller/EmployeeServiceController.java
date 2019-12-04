54877682ce6c4110d02b4239272146a6b9ed6daa
Kiran Verghese <kiran13rk@gmail.com>
2019-12-04 23:31:16 +0530

    fixed json

--- employee-service/src/main/java/com/application/employeeservice/controller/EmployeeServiceController.java
+++ employee-service/src/main/java/com/application/employeeservice/controller/EmployeeServiceController.java
@@ -1,24 +1,26 @@
 package com.application.employeeservice.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.application.employeeservice.dao.EmployeeServiceDao;
 import com.application.employeeservice.model.Employee;
 
 @RestController
+@RequestMapping("/employee")
 public class EmployeeServiceController {
 
 	@Autowired
 	private EmployeeServiceDao employeeServiceDao;
 	
-	@RequestMapping(value="/getEmployees",method = RequestMethod.GET)
+	@GetMapping("/getEmployees")
 	public List<Employee> getEmployees(){
-		
+		List<Employee> employeeList = (List<Employee>) employeeServiceDao.findAll();
+		System.out.println(employeeList);
+		return employeeList;
 	}
 }
--- employee-service/src/main/java/com/application/employeeservice/model/Employee.java
+++ employee-service/src/main/java/com/application/employeeservice/model/Employee.java
@@ -15,7 +15,7 @@ import lombok.ToString;
 
 @Entity
 @Table(name = "Employee")
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+// @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class Employee {
 
 	@Id
@@ -34,4 +34,116 @@ public class Employee {
 	
 	@Column(name = "employee_salary")
 	private Integer employeeSalary;
+
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public Integer getEmployeeAge() {
+		return employeeAge;
+	}
+
+	public void setEmployeeAge(Integer employeeAge) {
+		this.employeeAge = employeeAge;
+	}
+
+	public String getEmployeeRole() {
+		return employeeRole;
+	}
+
+	public void setEmployeeRole(String employeeRole) {
+		this.employeeRole = employeeRole;
+	}
+
+	public Integer getEmployeeSalary() {
+		return employeeSalary;
+	}
+
+	public void setEmployeeSalary(Integer employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+
+	public Employee(Integer employeeId, String employeeName, Integer employeeAge, String employeeRole,
+			Integer employeeSalary) {
+		super();
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		this.employeeAge = employeeAge;
+		this.employeeRole = employeeRole;
+		this.employeeSalary = employeeSalary;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + ", employeeAge=" + employeeAge
+				+ ", employeeRole=" + employeeRole + ", employeeSalary=" + employeeSalary + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((employeeAge == null) ? 0 : employeeAge.hashCode());
+		result = prime * result + ((employeeId == null) ? 0 : employeeId.hashCode());
+		result = prime * result + ((employeeName == null) ? 0 : employeeName.hashCode());
+		result = prime * result + ((employeeRole == null) ? 0 : employeeRole.hashCode());
+		result = prime * result + ((employeeSalary == null) ? 0 : employeeSalary.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (employeeAge == null) {
+			if (other.employeeAge != null)
+				return false;
+		} else if (!employeeAge.equals(other.employeeAge))
+			return false;
+		if (employeeId == null) {
+			if (other.employeeId != null)
+				return false;
+		} else if (!employeeId.equals(other.employeeId))
+			return false;
+		if (employeeName == null) {
+			if (other.employeeName != null)
+				return false;
+		} else if (!employeeName.equals(other.employeeName))
+			return false;
+		if (employeeRole == null) {
+			if (other.employeeRole != null)
+				return false;
+		} else if (!employeeRole.equals(other.employeeRole))
+			return false;
+		if (employeeSalary == null) {
+			if (other.employeeSalary != null)
+				return false;
+		} else if (!employeeSalary.equals(other.employeeSalary))
+			return false;
+		return true;
+	}
+	
+	
 }
